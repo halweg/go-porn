8f342a8abc5c70cc81f248f5c86aed1d9a4dcce0
halweg <halweg@126.com>
2020-10-25 21:04:05 +0800

    register orm

--- demo/main.go
+++ demo/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	_ "demo/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/orm"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+    orm.RegisterDriver("mysql", orm.DRMySQL)
+    orm.RegisterDataBase("default", "mysql",  beego.AppConfig.String("defaultdb"))
 	beego.Run()
 }
 
